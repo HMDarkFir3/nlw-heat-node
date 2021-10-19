73e15fc2e93a5f7095b85697d1b19ff736f0f064
hmdarkfir3 <henriquestudo@outlook.com>
2021-10-19 19:54:55 -0300

    class one done

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
+import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken";
 
 interface IAccessTokenResponse {
   access_token: string;
 }
 
 interface IUserResponse {
-  login: string;
   id: number;
+  login: string;
   avatar_url: string;
   name: string;
 }
@@ -36,7 +38,41 @@ class AuthenticateUserService {
       }
     );
 
-    return response.data;
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return { token, user };
   }
 }
 
